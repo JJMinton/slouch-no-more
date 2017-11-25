d3364fe2423fa0d58da863ab55144e9ea2c710d5
JJMinton <jeremyminton@gmail.com>
2017-11-25 18:13:59 +0000

    code refactor

--- facial_tracking.py
+++ facial_tracking.py
@@ -1,54 +1,54 @@
 #!/bin/python3.6
-import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, json
+import requests, json
 
 import keys
 
-###############################################
-#### Update or verify the following values. ###
-###############################################
 
-# Replace the subscription_key string value with your valid subscription key.
-subscription_key = keys.key_face
+def should_analyze(image):
+    return True
 
-# Replace or verify the region.
-#
-# You must use the same region in your REST API call as you used to obtain your subscription keys.
-# For example, if you obtained your subscription keys from the westus region, replace 
-# "westcentralus" in the URI below with "westus".
-#
-# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-# a free trial subscription key, you should not need to change this region.
-uri_base = keys.url
+def get_image():
+    with open('images/test1.jpg', 'rb') as f:
+        data = f.read()
+    return data
+    
+def analyze_image(image_data):
+    try:
+        # Execute the REST API call and get the response.
+        response = requests.request('POST', keys.url_face, files={}, data=image_data, headers=headers, params=params)
+        parsed = json.loads(response.text)
+        return parsed
 
-# Request headers.
-headers = {
-    'Content-Type': 'application/octet-stream',
-    'Ocp-Apim-Subscription-Key': subscription_key,
-}
+    except Exception as e:
+        print('Error:')
+        print(e)
+    
 
-# Request parameters.
-params = {
-    'returnFaceId': 'false',
-    'returnFaceLandmarks': 'true',
-    'returnFaceAttributes': 'headPose,emotion',
-    #'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
-}
-
-# Body. The URL of a JPEG image to analyze.
-#body = {'url': 'http...'}
-with open('images/test1.jpg', 'rb') as f:
-    data = f.read()
-
-try:
-    # Execute the REST API call and get the response.
-    response = requests.request('POST', uri_base + '/face/v1.0/detect', files={}, data=data, headers=headers, params=params)
-
+def process_result(result):
     print ('Response:')
-    parsed = json.loads(response.text)
-    print (json.dumps(parsed, sort_keys=True, indent=2))
+    print (json.dumps(result, sort_keys=True, indent=2))
 
-except Exception as e:
-    print('Error:')
-    print(e)
+if __name__ == "__main__":
 
-####################################  
+    # Request headers.
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': keys.key_face,
+    }
+
+    # Request parameters.
+    params = {
+        'returnFaceId': 'false',
+        'returnFaceLandmarks': 'true',
+        'returnFaceAttributes': 'headPose,emotion',
+        #'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    }
+    
+    i=0
+    while i < 4:
+        i += 1
+        image = get_image()
+        if should_analyze(image):
+            result = analyze_image(image)
+            process_result(result)
+
