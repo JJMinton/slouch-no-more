345056d3e935c49b711a13fc08b443929d050019
JJMinton <jeremyminton@gmail.com>
2017-11-25 21:31:33 +0000

    bugfixes.

--- facial_tracking.py
+++ facial_tracking.py
@@ -5,6 +5,7 @@ import keys
 
 
 from webcam import get_image
+#from test_images import get_image
 from api_call import make_api_call
 from analyse_posture import analyse_posture
 
@@ -33,6 +34,6 @@ if __name__ == "__main__":
     while i < 4:
         i += 1
         image = get_image()
-        if should_analyze(image):
+        if should_analyse(image):
             api_result = make_api_call(image)
             analyse_posture(api_result)
